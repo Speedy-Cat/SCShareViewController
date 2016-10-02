73b6ebf36a2d629949a1c2884a2857a7c18b5cc2
Adrian Ortuzar <adrian.ortuzar@scaura.com>
2016-10-02 19:14:23 +0200

    fix layout

--- SCInbox/SCShareLayout.m
+++ SCInbox/SCShareLayout.m
@@ -31,17 +31,48 @@
     }
     
     
-    
-    
     return CGSizeMake(width, self.collectionView.frame.size.height);
+}
+
+
+-(NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect
+{
+    NSMutableArray* elementsInRect = [NSMutableArray array];
+    NSArray *layoutAttributes = self.layoutAttributes;
+    
+    for(NSUInteger i = 0; i < layoutAttributes.count; i++){
+        
+        UICollectionViewLayoutAttributes *attribute = (UICollectionViewLayoutAttributes*)layoutAttributes[i];
+        if(CGRectIntersectsRect(attribute.frame, rect))
+        {
+            [elementsInRect addObject:attribute];
+        }
+    }
+    
+    return elementsInRect;
+}
+
+
+-(UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewLayoutAttributes* attr = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+    
+    
+    return attr;
+}
+
+- (CGFloat)widthOfString:(NSString *)string withFont:(UIFont *)font {
+    NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:font, NSFontAttributeName, nil];
+    return [[[NSAttributedString alloc] initWithString:string attributes:attributes] size].width;
 }
 
 -(NSArray *)layoutAttributes
 {
     NSMutableArray* elementsInRect = [NSMutableArray array];
+    NSArray *contacts = ((SCMailsCollectionView *)self.collectionView).contacts;
     
-    for(NSUInteger i = 0; i < ((SCMailsCollectionView *)self.collectionView).contacts.count; i++){
-        id contact = ((SCMailsCollectionView *)self.collectionView).contacts[i];
+    for(NSUInteger i = 0; i < contacts.count; i++){
+        id contact = contacts[i];
         
         UICollectionViewLayoutAttributes *lastAtt;
         if(elementsInRect.count){
@@ -58,7 +89,10 @@
             
             if (lastAtt) {
                 CGFloat x = lastAtt.frame.origin.x + lastAtt.frame.size.width + marginRight;
-                CGFloat width = self.collectionView.frame.size.width - lastAtt.frame.size.width;
+                CGFloat width = self.collectionView.frame.size.width - lastAtt.frame.size.width - lastAtt.frame.origin.x - marginRight;
+                if (width < 200 ) {
+                    width = 200;
+                }
                 cellFrame = CGRectMake(x, 0, width, self.collectionView.frame.size.height);
             }
             else{
@@ -95,35 +129,6 @@
     return elementsInRect;
 }
 
--(NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect
-{
-    NSMutableArray* elementsInRect = [NSMutableArray array];
-    NSArray *layoutAttributes = self.layoutAttributes;
-    
-    for(NSUInteger i = 0; i < layoutAttributes.count; i++){
-        
-        UICollectionViewLayoutAttributes *attribute = (UICollectionViewLayoutAttributes*)layoutAttributes[i];
-        if(CGRectIntersectsRect(attribute.frame, rect))
-        {
-            [elementsInRect addObject:attribute];
-        }
-    }
-    
-    return elementsInRect;
-}
-
-- (CGFloat)widthOfString:(NSString *)string withFont:(UIFont *)font {
-    NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:font, NSFontAttributeName, nil];
-    return [[[NSAttributedString alloc] initWithString:string attributes:attributes] size].width;
-}
-
--(UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    UICollectionViewLayoutAttributes* attr = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
-    
-    
-    return attr;
-}
 
 
 @end
